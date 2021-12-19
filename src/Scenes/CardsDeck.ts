d272cb760e440d4ab0798d81b1d7cb926e7ad54f
Jefferson <jeffersonmiranda94@outlook.com>
2021-12-19 02:31:08 -0300

    added Cards Deck game

---
+++ src/Scenes/CardsDeck.ts
@@ -0,0 +1,128 @@
+import { Container, Sprite } from 'pixi.js'
+import gsap from 'gsap'
+import Scene from './Scene'
+import { Globals } from '../Globals'
+import { isMobile } from '../utils'
+import BackgroundSound from '../BackgroundSound'
+
+export default class CardsDeck extends Scene {
+  constructor() {
+    super()
+
+    this.createBackground()
+    this.cards.position.y = 0
+    this.cards.sortableChildren = true
+    this.addChild(this.cards)
+    this.createDeckCards().then(() => {
+      this.createSecondCards()
+    }) 
+
+    this.on('removed', () => {
+      this.cardsSound.stop()
+    })
+  }
+
+  private cards: Container = new Container()
+  private cardsSprites: Sprite[] = []
+  private cardsQuantity: number
+  private distanceBetweenCards: number
+  private lastZIndex: number = 0
+  private cardsSound: BackgroundSound
+
+  public createBackground(): void {
+    const backgroundSprite = Sprite.from(Globals.resources['tableCards'].texture)
+
+    backgroundSprite.width = window.innerWidth
+    backgroundSprite.height = window.innerHeight
+
+    this.addChild(backgroundSprite)
+  }
+
+  public playSound() {
+    this.cardsSound = new BackgroundSound('./../assets/sounds/Dealing-cards-sound.mp3')
+  }
+
+  public createRandomCard(): Sprite {
+    const cardsResourcesKeys = Object.keys(Globals.resources).filter((key: any) => key.startsWith('card'))
+    const randomIndex = cardsResourcesKeys[Math.floor(Math.random() * cardsResourcesKeys.length)]
+
+    return Sprite.from(Globals.resources[randomIndex].texture)
+  }
+
+  public createDeckCards(cardsQuantity: number = 144): Promise<unknown> {
+    this.playSound()
+    const responsiveDistanceBetweenCards = isMobile ? 3.5 : 2.5
+
+    this.distanceBetweenCards = responsiveDistanceBetweenCards
+    this.cardsQuantity = cardsQuantity
+    
+    let index = 0
+
+    return new Promise((resolve, reject) => {
+      const cardMoving = setInterval(() => {
+      const allCardsArePositioned = true
+      const cardSprite = this.createRandomCard()
+      this.cardsSprites.push(cardSprite)
+      this.addNewCardToDeck(cardSprite, index * this.distanceBetweenCards)  
+      
+      if (index >= this.cardsQuantity - 1) {
+        clearInterval(cardMoving)
+        resolve(allCardsArePositioned)
+        this.cardsSound.stop()
+      }
+      
+      index++
+      }, 30)
+    })
+  }
+  
+  public addNewCardToDeck(newCard: Sprite, positionY: number): void {
+    const responsivePositionY = isMobile ? window.innerHeight / 10 : window.innerHeight / 5
+    
+    const cardPositionY = window.innerHeight - responsivePositionY - positionY
+    const cardPositionX = isMobile ? 80 : 100
+    const cardSize = isMobile ? window.innerHeight / 1750 : 1
+    
+    newCard.position.set(-100, cardPositionY)
+    newCard.anchor.set(0.5)
+    newCard.scale.set(cardSize)
+    
+    this.cards.addChild(newCard)
+
+    const animationDuration = 1
+
+    gsap.to(newCard, animationDuration, { x: cardPositionX, ease: 'expo'}).play()
+  }
+
+  public moveCardToSecondDeck(card: any, positionY: number): void {
+    this.lastZIndex++
+    card.zIndex = this.lastZIndex
+
+    const screenHeight = window.innerHeight
+
+    const responsivePositionY = isMobile ?  
+                                screenHeight - (screenHeight / 10) - positionY : 
+                                screenHeight - (screenHeight / 5) - positionY
+
+    const cardPositionX = isMobile ? window.innerWidth - 80: window.innerWidth / 2
+    const animationDuration = 2
+
+    gsap.to(card, animationDuration, { x: cardPositionX, y: responsivePositionY}).play()
+  }
+
+  public createSecondCards(): void {
+    let index = this.cardsQuantity - 1
+    let indexPositionY = 0
+   
+    const cardMoving = setInterval(() => {
+      this.moveCardToSecondDeck(this.cards.children[index], indexPositionY * this.distanceBetweenCards)
+      
+      if (index === 0) {
+        clearInterval(cardMoving)
+      }
+      
+      index--
+      indexPositionY++
+    }, 2000)
+  }
+}
